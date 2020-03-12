7b3a001aaafb251d8ae79b0d44e3c94ac4cc3eff
psj-freelancer <41204463+psj-freelancer@users.noreply.github.com>
2020-03-12 12:18:12 +0530

    updated api url

--- config/client/development.js
+++ config/client/development.js
@@ -10,7 +10,7 @@ export default {
     },
     api: {
         atlas: {
-            baseUrl: 'http://localhost:8000/v1'
+            baseUrl: 'http://nicistore.com/api/v1'
         }
     },
     googleAnalytics: {
